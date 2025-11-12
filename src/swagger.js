4545a0e4c538fd9ff7c543ba1b9c78ca68b70687
TelegaNicolas <telega.nicolas.pro@gmail.com>
2025-11-12 18:13:20 +0100

    add(global):ajout du fichier swagger.js et de la mise en place du swagger dans la route formation.route.js

---
+++ src/swagger.js
@@ -0,0 +1,11 @@
+import swaggerJsdoc from "swagger-jsdoc";
+export const swaggerSpec = swaggerJsdoc({
+definition: {
+openapi: "3.0.0",
+info: {
+title: "Projet API Formations",
+version: "1.0.0"
+ }
+ },
+apis: ["./src/routes/*.js"]
+});
